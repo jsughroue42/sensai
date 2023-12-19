2464311c4d657786ea995747ffe3fca42534e57c
Jason Sughroue <j_sughroue@hotmail.com>
2023-12-18 23:55:08 -0800

    Thru CH11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,6 +19,8 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -26,7 +28,8 @@ export async function fetchRevenue() {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
-
+    console.log('Data fetch completed after 3 seconds.');
+    
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
+//import { arboria } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
@@ -7,3 +8,11 @@ export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
   });
+
+ // export const arboria = arboria();
+
+ //CSS for Arboria Light 
+ //font-family: arboria, sans-serif;
+ //font-style: normal;
+ //font-weight: 300;
+ // Arboria Book same but font-weight: 400;
