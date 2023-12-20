743e36645c0eee9cd1d98ad45c270ee875e76734
Jason Sughroue <j_sughroue@hotmail.com>
2023-12-19 16:33:11 -0800

    Ch14 complete, pre authentications

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,12 +1,16 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
     <div
-    > <img 
+    > <Image 
         src="/sensai.png" 
         alt="SensAI Logo" 
+        width={200}   // Replace with the actual width of the image
+      height={300}  // Replace with the actual height of the image
+      layout="responsive" // Optional: Makes the image responsive
     />
     </div>
   );
